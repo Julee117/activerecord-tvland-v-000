cd9f561fe0fa93af30979f9d0b7ef66f8930f1d1
Julianne Kim <juliannne@gmail.com>
2017-11-11 05:25:35 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,13 +5,4 @@ class Character < ActiveRecord::Base
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
-
-#   def build_show
-#     self.build_show(:name => "Firefly")
-#     self.save
-#   end
-#
-#   def build_network
-#     self.build_network(:call_letters => "Fox")
-#   end
 end
